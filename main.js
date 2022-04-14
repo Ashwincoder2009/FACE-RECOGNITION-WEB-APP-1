e03d74b9a247c2cccebfa442076c6d993de9956d
Ashwincoder2009 <93897345+Ashwincoder2009@users.noreply.github.com>
2022-04-14 19:52:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,17 @@ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2
 function modelLoaded(){
 console.log("modelLoaded");
 }
+function check(){
+img=document.getElementById("CapturedImage");
+classifier.classify(img,gotResult)
+}
+function gotResult(error,results){
+if(error){
+console.error(error)
+}
+else{
+console.log(results)
+document.getElementById("Result_Family").innerHTML=results[0].label;
+document.getElementById("Result_Accuracy").innerHTML=results[0].confidence.toFixed(3);
+}
+}
